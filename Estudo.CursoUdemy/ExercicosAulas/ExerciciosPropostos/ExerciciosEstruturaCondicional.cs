403d9bd116a65ff384a1905127850ff742094af2
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-06-22 00:35:58 -0300

    Adicionando Enunciado dos exercicios

--- Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosEstruturaCondicional.cs
+++ Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosEstruturaCondicional.cs
@@ -7,6 +7,7 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
     {
         public static void negativoPositivo()
         {
+            /*Fazer um programa para ler um número inteiro, e depois dizer se este número é negativo ou não.*/
 
             int n1 = Utilitarios.pedirNumeroInteiro();
 
@@ -27,6 +28,7 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void parImpar()
         {
+            /*Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar*/
             int n1 = Utilitarios.pedirNumeroInteiro();
 
             if (n1 % 2 == 0)
@@ -41,6 +43,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void multioplos()
         {
+            /*Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao
+            Multiplos", indicando se os valores lidos são múltiplos entre si. Atenção: os números devem poder ser digitados em
+            ordem crescente ou decrescente*/
             List<int> valores = Utilitarios.pedirDoisNumerosInteiros();
             int n1 = valores[0];
             int n2 = valores[1];
@@ -57,6 +62,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void horasJogo()
         {
+            /*Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode
+            começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.*/
             List<int> valores = Utilitarios.pedirDoisNumerosInteiros();
             int hrInicial = valores[0];
             int hrFinal = valores[1];
@@ -77,6 +84,15 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void valorPagar()
         {
+            /*Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+            Seguir, calcule e mostre o valor da conta a pagar.
+            
+            1  |  Cachorro quente   |  R$ 4.00   
+            2  |  X-salada          |  R$ 4.50
+            3  |  X-bacon           |  R$ 5.00
+            4  |  torrada simples   |  R$ 2.00
+            5  |  refrigerante      |  R$ 1.50
+            */
             List<int> valores = Utilitarios.pedirDoisNumerosInteiros();
             int n1 = valores[0];
             int n2 = valores[1];
@@ -113,6 +129,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void intervalo()
         {
+            /*Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos
+            seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em
+            nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.*/
             double n1 = Utilitarios.pedirNumeroDecimal();
 
             if (n1 >= 0 && n1 <= 25)
@@ -139,6 +158,12 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void quadrante()
         {
+            /*Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas
+            de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o
+            ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
+            Se o ponto estiver na origem, escreva a mensagem “Origem”.
+            Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a */
+
             List<double> valores = Utilitarios.pedirDoisNumerosDecimais();
             double x = valores[0];
             double y = valores[1];
@@ -167,6 +192,11 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void impostoDeRenda()
         {
+            /*Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+            que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+            qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+            Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+            mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.*/
             double n1 = Utilitarios.pedirNumeroDecimal("Digite seu salario: ");
 
             double imposto;
--- Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosEstruturaSequencial.cs
+++ Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosEstruturaSequencial.cs
@@ -7,7 +7,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
     {
         public static void soma()
         {
-            /**/
+            /*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+            mensagem explicativa, conforme exemplos*/
 
             Console.WriteLine("Digite um Numero: ");
             double n1 = double.Parse(Console.ReadLine());
@@ -23,7 +24,10 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void AreaCirculo()
         {
-            /**/
+            /*Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+            casas decimais conforme exemplos.
+            Fórmula da área: area = π . raio2
+            Considere o valor de π = 3.14159*/
             Console.WriteLine("Digite o valor de um raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
@@ -35,7 +39,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void DiferencaProduto()
         {
-            /**/
+            /*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+            de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
             Console.WriteLine("Digite 4 valores separados por espaços ex:( 1 4 2 6 ): ");
             string[] valores = Console.ReadLine().Split(' ');
 
