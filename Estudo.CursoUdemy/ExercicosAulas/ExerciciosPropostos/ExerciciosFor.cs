ec34f8e106952e4b8c5e044de8dae990340f45c3
Raian Medeiros Nolaço <81272272+RaianNolaco@users.noreply.github.com>
2022-06-23 14:20:11 -0300

    Adicionando enunciado dos exercicios

--- Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosFor.cs
+++ Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosFor.cs
@@ -11,6 +11,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
     {
         public static void impares()
         {
+            /*Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+            X, se for o caso.*/
             int n1 = Utilitarios.pedirNumeroInteiro("Digite um numero inteiro maio q 0 e menos que 1000: ");
 
             for (int i = 1; i<=n1; i++)
@@ -24,6 +26,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void intervalo()
         {
+            /*Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+            Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+            essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo)*/
             int n1 = Utilitarios.pedirNumeroInteiro("Quantos valores deseja incluir? ");
 
             int dentro = 0;
@@ -45,8 +50,11 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void testes()
         {
+            /*Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+            de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+            conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+            peso 5.*/
             int qntTestes = Utilitarios.pedirNumeroInteiro("Digite a quantidade de testes para processar: ");
-
 
             List<double> medias = new List<double>();
             for (int i = 1; i <= qntTestes; i++)
@@ -66,6 +74,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void divisao()
         {
+            /*Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+            segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel".*/
             int qntDivisoes = Utilitarios.pedirNumeroInteiro("Quantas divisões você quer fazer? ");
 
             for (int i = 1; i <= qntDivisoes; i++)
@@ -84,6 +94,8 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void fatorial()
         {
+            /*Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+            Lembrando que, por definição, fatorial de 0 é 1.*/
             int n1 = Utilitarios.pedirNumeroInteiro("Digigite um numero para calcular seu fatorial: ");
 
             int calc = 1;
@@ -96,6 +108,7 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void divisores()
         {
+            /*Ler um número inteiro N e calcular todos os seus divisores.*/
             int n1 = Utilitarios.pedirNumeroInteiro();
 
             for (int i = 1; i <= n1;i++)
@@ -109,6 +122,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExercicosAulas.ExerciciosPropostos
 
         public static void quadradoECubo()
         {
+            /*Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas,
+            começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme
+            exemplo.*/
             int n1 = Utilitarios.pedirNumeroInteiro();
 
             for (int i = 1; i <= n1; i++)
--- Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosWhile.cs
+++ Estudo.CursoUdemy/ExercicosAulas/ExerciciosPropostos/ExerciciosWhile.cs
@@ -7,6 +7,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
     {
         public static void senha()
         {
+            /*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+            incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+            impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.*/
             bool certo = true;
             while (certo)
             {
@@ -23,6 +26,9 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void quadranteqWhile()
         {
+            /*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+            cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+            menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).*/
             bool sair = true;
             while (sair)
             {
@@ -58,6 +64,12 @@ namespace Estudo.CursoUdemy.RecapLogica.ExerciciosAulas.ExerciciosPropostos
 
         public static void postoDeGasolina()
         {
+            /*Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+            um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
+            4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
+            que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
+            mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
+            exemplo.*/
             bool sair = true;
             int alcool = 0;
             int gasolina = 0;
