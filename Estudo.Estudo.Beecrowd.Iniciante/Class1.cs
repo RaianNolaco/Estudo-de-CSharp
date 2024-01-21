b8a2b933a47e45806eeb8e0c0ab8fa6005a03558
Raian Medeiros <raiannolaco@gmail.com>
2024-01-21 20:37:40 -0300

    ...

---
+++ Estudo.Estudo.Beecrowd.Iniciante/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.Estudo.Beecrowd.Iniciante
+{
+    public class Class1
+    {
+        static int Id { get; set; }
+
+    }
+}
