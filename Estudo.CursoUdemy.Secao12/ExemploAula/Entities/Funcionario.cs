5b8a0c46f8c082d0939ca62d1b3b57c88a3892f7
RaianNolaco <raiannolaco@gmail.com>
2025-10-29 00:53:12 -0300

    Interface IComparable

---
+++ Estudo.CursoUdemy.Secao12/ExemploAula/Entities/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Estudo.CursoUdemy.Secao12.ExemploAula.Entities
+{
+    public class Funcionario: IComparable
+    {
+        public string? Nome { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string csv) {
+            string[] vect = csv.Split(',');
+            Nome = vect[0];
+            Salario = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Nome +", "+ Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (!(obj is Funcionario))
+                throw new ArgumentException("Apenas funcionarios");
+
+            Funcionario outro = obj as Funcionario;
+            return Salario.CompareTo(outro.Salario);
+        }
+    }
+}
---
+++ Estudo.CursoUdemy.Secao12/ExemploAula/InterfaceIComparable.cs
@@ -0,0 +1,42 @@
+﻿using Estudo.CursoUdemy.Secao12.ExemploAula.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.CursoUdemy.Secao12.ExemploAula
+{
+    public static class InterfaceIComparable
+    {
+        public static void executar()
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> list = new List<Funcionario>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Funcionario(sr.ReadLine()));
+                    }
+
+                    list.Sort();
+
+                    foreach (Funcionario item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro: " + e.Message);
+            }
+        }
+    }
+}
