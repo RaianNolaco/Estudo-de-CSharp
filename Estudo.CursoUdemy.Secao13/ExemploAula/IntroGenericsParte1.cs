3b835cc06b17ccdebce9c386fbc503e155869a95
RaianNolaco <raiannolaco@gmail.com>
2025-10-30 01:56:15 -0300

    Aula sobre generics

---
+++ Estudo.CursoUdemy.Secao13/ExemploAula/IntroGenericsParte1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.CursoUdemy.Secao13.ExemploAula
+{
+    public static class IntroGenericsParte1
+    {
+        public static void executar()
+        {
+            PrintServiceGeneric<int> print = new PrintServiceGeneric<int>();
+
+            Console.Write("How many values? ");
+            int qntValues = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < qntValues; i++) {
+                int value = Convert.ToInt32(Console.ReadLine());
+                print.addValue(value);
+            }
+
+            print.print();
+            print.first();
+
+            PrintServiceGeneric<string> printString = new PrintServiceGeneric<string>();
+
+            Console.Write("How many values? ");
+            int qntValues2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < qntValues; i++) {
+                string value = Console.ReadLine();
+                printString.addValue(value);
+            }
+
+            printString.print();
+            printString.first();
+
+            print.dataType(); 
+            printString.dataType();
+        }
+    }
+}
---
+++ Estudo.CursoUdemy.Secao13/ExemploAula/PrintServiceGeneric.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo.CursoUdemy.Secao13.ExemploAula
+{
+    public class PrintServiceGeneric<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+        public T info { get; set; }
+        public void addValue(T valor)
+        {
+            if (_count == 10)
+                throw new InvalidOperationException("Print service is full");
+
+            _values[_count] = valor;
+            _count++;
+        }
+
+        public void first()
+        {
+            if (_count == 0)
+                throw new InvalidOperationException("Print service is empty");
+
+            Console.WriteLine("First: " + _values[0]);
+        }
+
+        public void print()
+        {
+            string valorPirnt = "[";
+            for (int i = 0; i < _count-1; i++)
+            {
+                valorPirnt += _values[i] + ", ";
+            }
+            
+            if(_count > 0)
+            {
+                valorPirnt += _values[_count - 1];
+            }
+
+            Console.WriteLine(valorPirnt + "]");
+        }
+
+        public void dataType()
+        {
+            Console.WriteLine(_values.GetType());           
+        }
+
+        public void printInfo()
+        {
+            Console.WriteLine($"Info: {info} é do tipo {info.GetType()}");
+        }
+    }
+}
